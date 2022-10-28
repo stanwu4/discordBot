19228f3976732cf9c88c5bf8ab1b18a8371790eb
stanwu4 <113301955+stanwu4@users.noreply.github.com>
2022-10-27 20:04:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord = require('Discord.js');
+const client = new Discord.client();
+client.once('ready', () =>{
+    console.log('Bot is online!')
+});
+client.login('MTAzNTMzODUwODExNTY0ODYwMg.GAPXeE.AyFLepdEf-doQfmp_zfXT3UrKqTRPdg-_z2498');
